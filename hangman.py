f8fcf590bdb4918c0d960ac72def32c7fcb56790
gr00vytvniks <mitchmackert@gmail.com>
2025-01-22 15:54:15 -0800

    commented out infinite loop

--- hangman.py
+++ hangman.py
@@ -5,8 +5,9 @@ import string
 
 def get_valid_word(words):
     word = random.choice(words) # randomly chooses something from the list
-    while '-' in word or '' in word:
-        word = random.choice(words)
+    # while '-' in word or '' in word:
+    #     word = random.choice(words)
+
 
     return word.upper()
 
